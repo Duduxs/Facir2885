3310f22f32f6ddc493c3b0f18e69412ad31b56da
Duduxs <53549133+Duduxs@users.noreply.github.com>
2019-09-13 13:28:44 -0300

    Create Atividade 5.c

---
+++ Atividades Lista3/Atividade 5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+/**
+@author Eduardo José
+@version 1.0.0
+@since 13/09/19
+*/
+
+
+int main(){
+
+int n1,n2,n3;
+
+printf("Valor 1: ");
+scanf("%i", &n1);
+
+printf("Valor 2: ");
+scanf("%i", &n2);
+
+printf("Valor 3: ");
+scanf("%i", &n3);
+
+if(n1 > n2 && n1 > n3){
+
+    printf("%i e maior!", n1);
+}
+
+else if (n2 > n1 && n2 > n3){
+    printf("%i e maior!", n2);
+}
+
+else{
+    printf("%i e maior!", n3);
+}
+
+
+
+
+}
