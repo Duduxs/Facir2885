5c1bb3595e5653c2a9c07fca68f486298c74107f
Duduxs <53549133+Duduxs@users.noreply.github.com>
2019-09-13 14:31:33 -0300

    Create Atividade 17.c

---
+++ Atividades Lista3/Atividade 17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+@author Eduardo José
+@version 1.0.0
+@since 13/09/19
+*/
+
+
+int main(){
+
+int n;
+
+printf("Diga n: ");
+scanf("%i", &n);
+
+for(int i = 1 ; i <= n ; i++){
+    printf("%i \n", i);
+}
+
+}
