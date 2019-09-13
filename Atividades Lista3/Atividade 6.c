082f27fc7b6e926a6e628a211f7f99786a25c445
Duduxs <53549133+Duduxs@users.noreply.github.com>
2019-09-13 13:32:53 -0300

    Create Atividade 6.c

---
+++ Atividades Lista3/Atividade 6.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+/**
+@author Eduardo José
+@version 1.0.0
+@since 13/09/19
+*/
+
+
+int main(){
+
+int n1,n2,n3;
+
+printf("Valor 1: ");
+scanf("%i", &n1);
+
+printf("Valor 2: ");
+scanf("%i", &n2);
+
+printf("Valor 3: ");
+scanf("%i", &n3);
+
+if(n1 > n2 & n1 > n3){
+
+    if( n2 > n3){
+        printf("Soma dos maiores: %i", n1+n2);
+    }
+        else{
+        printf("Soma dos maiores: %i", n1+n3);
+        }
+}
+
+if(n2 > n1 & n2 > n3){
+
+    if( n1 > n3){
+        printf("Soma dos maiores: %i", n2+n1);
+    }
+        else{
+        printf("Soma dos maiores: %i", n2+n3);
+        }
+}
+
+if(n3 > n2 & n3 > n1){
+
+    if( n2 > n1){
+        printf("Soma dos maiores: %i", n3+n2);
+    }
+        else{
+        printf("Soma dos maiores: %i", n3+n1);
+        }
+}
