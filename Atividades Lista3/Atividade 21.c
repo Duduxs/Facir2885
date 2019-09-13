33456419b92bc96ead9e289a484314ec3e576980
Duduxs <53549133+Duduxs@users.noreply.github.com>
2019-09-13 14:44:39 -0300

    Create Atividade 21.c

---
+++ Atividades Lista3/Atividade 21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+@author Eduardo José
+@version 1.0.0
+@since 13/09/19
+*/
+
+
+int main(){
+
+
+for(int i = 1 ; i <= 10 ; i++){
+
+printf("%i, 1 2 3 4 5 6 7 8 9 10 \n", i);
+}
+}
