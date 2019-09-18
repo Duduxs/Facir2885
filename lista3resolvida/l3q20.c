e953378d5e420e9d79cff7a6a9f53a8b69b687d4
Duduxs <duduxss3@gmail.com>
2019-09-18 17:51:30 -0300

    Rename

--- lista3resolvida/l3q20.c
+++ lista3resolvida/l3q20.c
@@ -2,7 +2,7 @@
 
 
 /**
-@author Eduardo JosÃ©
+@author Eduardo José
 @version 1.0.0
 @since 13/09/19
 */
@@ -10,13 +10,13 @@
 
 int main(){
 
-int soma, media, i,qtd;
+int soma = 0 , media, i,qtd = 0;
 
 for(i = 10 ; i <= 100 ; i+= 2){
-
 soma+=i;
 qtd++;
 }
+
 media = soma/qtd;
 printf("%i", media);
 }
